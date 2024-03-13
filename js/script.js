bdd60fd36180de38e958f203747a726c1b96fcd7
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-03-13 14:50:06 +0100

    Fix typos and add error handling in index.html and script.js

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ const app = createApp({
     return {
       todo:[
 
-        {title: 'Learn Vue', done: false},
-        {title: 'Learn React', done: true},
-        {title: 'Learn Angular', done: false},
-        {title: 'Learn Angular', done: true},
+        {title: 'Ripassare CSS', done: false},
+        {title: 'Coprare la Pappa a Eden', done: true},
+        {title: 'Insultare Alex Sibiriu', done: false},
+        
 
       ],
       newTodo: '',
@@ -21,18 +21,24 @@ const app = createApp({
 
           if (this.newTodo.length >= 4) {
             this.todo.push({
-            title: this.newTodo,
-            done: false
-          });
-          }else {
-            this.error= "Attenzione metti almeno una parola di 4 lettere!";
+              title: this.newTodo,
+              done: false,
+            });
+            this.newTodo = '';
+          } else {
+            this.error = "Attenzione metti almeno una parola di 4 lettere!";
           }  
           
           
         },
 
         removeTask (index) {
-          this.todo.splice(index, 1);
+          if (this.todo[index].done == true) {
+            this.todo.splice(index, 1);
+          }else {
+            this.error= "Attenzione non puoi eliminare un task non completata!";
+          }
+          
         },
       }
     }).mount('#app');
